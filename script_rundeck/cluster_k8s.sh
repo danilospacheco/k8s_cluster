30eb4cb58411a290657db5d18b45bf0a60591741
Danilo Silva Pacheco <danilo.pacheco@telefonica.com>
2022-09-14 18:45:08 -0300

    ajuste arquivos kube

--- script_rundeck/cluster_k8s.sh
+++ script_rundeck/cluster_k8s.sh
@@ -3,7 +3,7 @@ QAADM_KEY=/opt/web/ansible/production/clone/.key/teste2.pem
 ANSIBLE="sudo docker exec -it ansible2.3 ansible-playbook -i"
 #ESTEIRA="pre-prod"
 YML_DIR="playbooks"
-DOMAIN="/opt/web/ansible/production/k8s_cluster/kubernetes-and-ansible/${YML_DIR}/"
+DOMAIN="/opt/web/ansible/production/k8s_cluster/${YML_DIR}/"
 ADM="main-setting-kubernetes.yaml"
 INV="inv"
 
